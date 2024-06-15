09e6e1ced599f952b84abf174821966dbaf91523
Krishna Kumawat <127647086+K16Visionary@users.noreply.github.com>
2024-06-15 15:08:43 +0000

    Primitive & non Primitive datatype

---
+++ J.S/Nonprimitivedatatype.js/nonpromitive.js
@@ -0,0 +1,10 @@
+//Non primitive data type:object
+const sum = 2;
+const poornima_university = {
+    CyberSecurity:"Sec i j K",
+    Semister:2,
+    Students_in_cyber:120,
+    Overallrating:9,
+    sum:sum+9
+}
+console.log(poornima_university['Semister'])
