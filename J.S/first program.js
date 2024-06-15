3ed0e0ea9415e0424a171f390b4b703234deb7d0
Krishna Kumawat <127647086+K16Visionary@users.noreply.github.com>
2024-06-15 14:48:07 +0000

    first js program

---
+++ J.S/first program.js
@@ -0,0 +1,15 @@
+// our first js program
+let Name = "krishna kumawat";
+console.log(Name)
+
+// data types in js
+let string = "Krishna"; //string
+let number = 22; //number
+let boolean = true; //boolean values true and false
+let nullvalue = null; //this varible contain empty value
+let undefinedvalue = undefined; //means we dont know what type of value does it contain
+console.log("String value : ",string)
+console.log("number value : ",number)
+console.log("boolean value : ",boolean)
+console.log("nullvalue value : ",nullvalue)
+console.log("undefinedvalue value : ",undefinedvalue)
