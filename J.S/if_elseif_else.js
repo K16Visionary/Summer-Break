e15bb4d240cd9fe52e77440f0967f106d593d442
Krishna Kumawat <127647086+K16Visionary@users.noreply.github.com>
2024-06-17 14:40:22 +0000

    If, elseif ,else questionns

---
+++ J.S/if_elseif_else.js
@@ -0,0 +1,12 @@
+// if elseif else program to find the greatest number
+
+let num1 = prompt("Enter number 1 : ");
+let num2 = prompt("Enter number 2 : ");
+
+if(num1>num2){
+    console.log(num1,": is greater than :",num2);
+} else if(num1===num2) {
+    console.log(num1,": is Equal to :",num2);
+} else {
+    console.log(num2,": is greater than :",num1);
+}
