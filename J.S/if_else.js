dcb333212794c38cc28771ac3c722c4c26fbf598
Krishna Kumawat <127647086+K16Visionary@users.noreply.github.com>
2024-06-17 14:28:06 +0000

    If else questionns

---
+++ J.S/if_else.js
@@ -0,0 +1,8 @@
+//Program to check if a given number is even or not
+
+let number = prompt("Enter a number : ");
+if(number%2===0){
+    console.log("Given number :",number,"is even")
+} else {
+    console.log("Given number :",number,"is odd")
+}
