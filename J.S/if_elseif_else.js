d21aead8b9c726bbee54ffa4ee97e5b9376f53c4
Krishna Kumawat <127647086+K16Visionary@users.noreply.github.com>
2024-06-19 16:04:14 +0000

    practise of js

--- J.S/if_elseif_else.js
+++ J.S/if_elseif_else.js
@@ -9,4 +9,4 @@ if(num1>num2){
     console.log(num1,": is Equal to :",num2);
 } else {
     console.log(num2,": is greater than :",num1);
-}
+} 
---
+++ J.S/table.js
@@ -0,0 +1,7 @@
+//Program to print a table of a number
+
+let num = parseInt(prompt("Enter a number : "));
+for(let i = 1; i<=10;i++) {
+    const result = i * num;
+    console.log(result);
+}
